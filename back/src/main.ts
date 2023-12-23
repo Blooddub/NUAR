20ed36dcd9c8f92ec05c4fb7ab30f34764e5e371
LawRok <destrosd@gmail.com>
2023-12-23 23:14:46 +0500

    commit
    tryConnectDB

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    // connect to db
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    await app.listen(3000, () => {
+        console.log("Server started")
+    });
 }
 bootstrap();
